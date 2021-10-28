3240de658381836fab280ccf1c2a57169332d194
Jason Stillerman <jason.t.stillerman@gmail.com>
2021-10-27 22:52:45 -0400

    Print out exceptions when scraping

--- Step2/download_images.py
+++ Step2/download_images.py
@@ -1,7 +1,6 @@
 import urllib.request
-# imgURL = "http://site.meishij.net/r/58/25/3568808/a3568808_142682562777944.jpg"
-
 import json
+
 json_file_path = "../Step1.json"
 
 with open(json_file_path, 'r') as j:
@@ -11,5 +10,4 @@ with open(json_file_path, 'r') as j:
         try:
             urllib.request.urlretrieve(urls[i], f"./images/{i}.jpg")
         except Exception as e:
-            print ("Failed to get #", i, "with url", urls[i])
-
+            print ("Failed to get #", i,e)
