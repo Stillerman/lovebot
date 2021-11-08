cf946c6efaf03119a76c80f1ab9d647305a1e80e
Jason Stillerman <jason.t.stillerman@gmail.com>
2021-11-08 11:06:35 -0500

    Tweaked OpenAI

--- Step5-OpenAI/GenerateConnection.py
+++ Step5-OpenAI/GenerateConnection.py
@@ -6,17 +6,6 @@ from MakeImg import createImage
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def makeConnection(name):
-    # response = openai.Completion.create(
-    # engine="davinci",
-    # prompt=f"to the boy building the ski slope\noutside of UHS, i made kissy faces\nat you out the window and u were\nstaring. i miss u\n---\nJoe from L&L B plz kiss me hehe\n---\nOliver long has my heart. Don't\nwant to tell him\n---\nTo the boy that drives the red truck with\nIllinois plates that I keep seeing in the\nparking lot: hi you're cute & I like your\ntruck\n---\n{name}",
-    # temperature=1,
-    # max_tokens=64,
-    # top_p=0.51,
-    # frequency_penalty=0,
-    # presence_penalty=0,
-    # stop=["---"]
-    # )
-
     response = openai.Completion.create(
         engine="davinci",
         prompt=f"to the guy with iowa plates we saw driving\n(3/14) sorry for tailgating we just really\nwanted to know what spotify song you had\nas a sticker\n---\nJoe from L&L B plz kiss me hehe\n---\nOliver long has my heart. Don't\nwant to tell him\n---\nMorgan you have such a\nwonderful energy and I just wanna\ngo look at moths with you\n---\nBlondie I keep seeing in RowellA\nshe could 100% pull me\n---\n{name}",
@@ -33,4 +22,4 @@ def makeConnection(name):
 
 message = makeConnection(" ".join(sys.argv[1:]) + " ")
 
-createImage(message)
+createImage(" ".join(message.split("\n")))
--- Step5-OpenAI/MakeImg.py
+++ Step5-OpenAI/MakeImg.py
@@ -3,7 +3,21 @@ import numpy as np
 import math
 import random
 
-def createImage(text):
+def createImage(text, returnImg = False):
+  lines = []
+  current_line = ""
+  max_chars = 35
+
+  for word in text.split(" "):
+    if (len(word) + len(current_line) < max_chars):
+      current_line += word + " "
+    else:
+      lines.append(current_line)
+      current_line = word + " "
+  lines.append(current_line)
+
+  text = "\n".join(lines)
+
   dims = (1000, 1000)
   (ydim, xdim) = dims
 
@@ -25,12 +39,13 @@ def createImage(text):
   line_type = cv2.LINE_AA
 
   text_size, _ = cv2.getTextSize(text, font, font_scale, thickness)
-  line_height = text_size[1] + 5
-  text_height = text_size[1] * len(text.split("\n"))
+  line_height = text_size[1] + 10
+  text_height = line_height * (len(text.split("\n")) + 1)
+  half_text_height = math.floor(text_height / 2)
 
-  img[500-text_height:500+text_height,15:1000-15,:] = 1
+  img[500-half_text_height:500+half_text_height,15:1000-15,:] = 1
 
-  position = (30, 500 - math.floor(0.5 * text_height))
+  position = (30, 500 - half_text_height + line_height)
 
   x, y0 = position
   for i, line in enumerate(text.split("\n")):
@@ -44,6 +59,7 @@ def createImage(text):
                   thickness,
                   line_type)
 
-  cv2.imshow("j", img)
-  cv2.waitKey(0)
-  cv2.destroyAllWindows()
+  if (returnImg):
+    return img*255.0
+
+  cv2.imwrite("tada.png", img * 255.0)
